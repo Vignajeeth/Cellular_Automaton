72fdfc8255ab148219c3e889b5203310e4526580
Vignajeeth <vignajeet.h@gmail.com>
2021-05-17 16:39:50 -0700

    Added repr and minor changes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ height, width = 800, 800
 tot_rows, tot_columns = 20, 20
 gap_x, gap_y = width // tot_columns, height // tot_rows
 WIN = pygame.display.set_mode((height, width))
+delta_time = 125
 
 
 class Game_Of_Life:
@@ -16,18 +17,24 @@ class Game_Of_Life:
 
 class Cell:
 
-    colour_map = {True: "BLACK", False: "WHITE"}
+    color_map = {True: "BLACK", False: "WHITE"}
 
-    def __init__(self, row_no, col_no, colour="WHITE", val=False) -> None:
+    def __init__(self, row_no, col_no, color="WHITE", val=False) -> None:
         self.pos_x = int(row_no * gap_x)
         self.pos_y = int(col_no * gap_y)
-        self.colour = colour
+        self.color = color
         self.val = val
 
     def draw_cell(self, WIN):
         pygame.draw.rect(
-            WIN, self.colour, (self.pos_x, self.pos_y, width / 8, height / 8)
+            WIN, self.color, (self.pos_x, self.pos_y, width / 8, height / 8)
         )
+
+    def __repr__(self):
+        if self.val:
+            return "  X  "
+        else:
+            return "  _  "
 
 
 class Matrix:
@@ -35,12 +42,7 @@ class Matrix:
         self.matrix = self.create_matrix()
 
     def create_matrix(self):
-        grid = []
-        for i in range(tot_rows):
-            grid.append([])
-            for j in range(tot_columns):
-                node = Cell(j, i)
-                grid[i].append(node)
+        grid = [[Cell(j, i) for j in range(tot_columns)] for i in range(tot_rows)]
         return grid
 
     def draw_matrix(self, win):
@@ -84,10 +86,10 @@ class Matrix:
                 count = 0
                 for i in neighbours:
                     row, col = i
-                    if self.matrix[row][col].colour == "BLACK":
+                    if self.matrix[row][col].color == "BLACK":
                         count += 1
 
-                if tile.colour == "BLACK":
+                if tile.color == "BLACK":
                     if count in [2, 3]:
                         newgrid.append("BLACK")
                     else:
@@ -101,15 +103,25 @@ class Matrix:
 
         return newgrid
 
+    def __repr__(self) -> str:
+        result = ""
+        for i in range(len(self.matrix)):
+            result += "\n"
+            for j in range(len(self.matrix[0])):
+                result += str(self.matrix[i][j])
+        return result
+
 
 def driver():
     run = False
     grid = Matrix()
 
     while True:
-        pygame.time.delay(125)
+        pygame.time.delay(delta_time)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_q
+            ):
                 pygame.quit()
                 sys.exit()
 
@@ -120,25 +132,29 @@ def driver():
                 pos = pygame.mouse.get_pos()
                 row, col = pos[0] // gap_y, pos[1] // gap_x
                 grid.matrix[col][row].val = not grid.matrix[col][row].val
-                grid.matrix[col][row].colour = grid.matrix[col][row].colour_map[
+                grid.matrix[col][row].color = grid.matrix[col][row].color_map[
                     grid.matrix[col][row].val
                 ]
 
             while run:
-                pygame.time.wait(250)
+                pygame.time.wait(delta_time)
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                         run = False
+                        # print(grid)
 
-                newcolours = grid.update_grid()
+                newcolors = grid.update_grid()
                 count = 0
                 for i in range(len(grid.matrix)):
                     for j in range(len(grid.matrix[0])):
-                        grid.matrix[i][j].colour = newcolours[count]
+                        grid.matrix[i][j].color = newcolors[count]
                         count += 1
                 grid.update_display(WIN)
             grid.update_display(WIN)
 
 
 if __name__ == "__main__":
+    # Click to Toggle Cells.
+    # Spacebar for start/stop.
+    # q for Quitting.
     driver()
