fe8c8be55707517033b1a47b4881aa4eda22a0ab
Vignajeeth <vignajeet.h@gmail.com>
2021-05-18 23:48:46 -0700

    Minor changes and comments.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
 import sys
 import scipy.signal as sp
-
-# TODO Clean Code up
+import numpy as np
 
 
 class Cell:
     """
-    Useless as of now.
+    Basic cell in the matrix which tracks True/False and color.
     """
 
     color_map = {True: "BLACK", False: "WHITE"}
@@ -24,20 +23,33 @@ class Cell:
 
 
 class Matrix:
+    """
+    Performs all the operations related to matrix manipulation.
+    """
+
     def __init__(self, height=800, width=800, rows=40, columns=40) -> None:
+        """
+        Initialises dimensions, grid, filters and surface to draw.
+        """
+        """Editable"""
         self.height, self.width = height, width
         self.total_rows, self.total_columns = rows, columns
         self.gap_x, self.gap_y = (
             self.width // self.total_columns,
             self.height // self.total_rows,
         )
-        # Class Cell is not used. May require later.
+        """Non Editable"""
         self.even_matrix = [[False for j in range(columns)] for i in range(rows)]
         self.odd_matrix = [[False for j in range(columns)] for i in range(rows)]
         self.even = True
+        self.moore_filter = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        self.von_neumann_filter = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
 
+        """Display"""
         self.WIN = pygame.display.set_mode((height, width))
         pygame.draw.rect(self.WIN, "WHITE", (0, 0, height, width))
+
+        """Horizontal and vertical lines"""
         for i in range(columns):
             gap = i * self.gap_x
             pygame.draw.line(self.WIN, "BLACK", (gap, 0), (gap, height))
@@ -47,9 +59,15 @@ class Matrix:
         pygame.display.update()
         # pygame.draw.line(WIN, "BLACK", (100, 0), (200, 200))
 
+    def get_population(self, generation):
+        """
+        Returns the population of the world at the given generation.
+        """
+        return np.sum(self.even_matrix if generation % 2 == 0 else self.odd_matrix)
+
     def manual_update(self, pos):
         """
-        Used when updated with a mouse.
+        Used when updated with a mouse. Changes Boolean in matrix and draws Rect.
         """
         color_map = {True: "BLACK", False: "WHITE"}
         x = pos[0] // self.gap_x
@@ -67,72 +85,25 @@ class Matrix:
 
     def increment_time_step(self, birth, survival):
         """
-        Used for incrementing the time step and changing matrices.
+        Increments the generation and repopulates the matrix.
+        Has 2 matrices which serve as the previous state for each other.
         """
         birth = [int(i) for i in birth]
         survival = [int(i) for i in survival]
-        filter = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+
         curr_matrix, next_matrix = (
             (self.even_matrix, self.odd_matrix)
             if self.even
             else (self.odd_matrix, self.even_matrix)
         )
+        """Creating neighbour matrix directly using scipy for speed."""
         neighbour_matrix = sp.correlate2d(
-            curr_matrix, filter, mode="same", boundary="wrap"
+            curr_matrix, self.moore_filter, mode="same", boundary="wrap"
         )
+
+        """Update rule logic"""
         for i in range(self.total_rows):
             for j in range(self.total_columns):
-                # neighbours = (
-                #     np.sum(curr_matrix[i - 1 : i + 2][j - 1 : j + 2])
-                #     - curr_matrix[i][j]
-                # )
-
-                # if i == self.total_rows - 1 and j == self.total_columns - 1:
-                #     neighbours = (
-                #         curr_matrix[i - 1][j - 1]
-                #         + curr_matrix[i][j - 1]
-                #         + curr_matrix[i - 1][j]
-                #         + curr_matrix[i][0]
-                #         + curr_matrix[i - 1][0]
-                #         + curr_matrix[0][j]
-                #         + curr_matrix[0][j - 1]
-                #         + curr_matrix[0][0]
-                #     )
-                # elif i == self.total_rows - 1:
-                #     neighbours = (
-                #         curr_matrix[i - 1][j - 1]
-                #         + curr_matrix[i][j - 1]
-                #         + curr_matrix[0][j - 1]
-                #         + curr_matrix[i - 1][j]
-                #         + curr_matrix[0][j]
-                #         + curr_matrix[i - 1][j + 1]
-                #         + curr_matrix[i][j + 1]
-                #         + curr_matrix[0][j + 1]
-                #     )
-
-                # elif j == self.total_columns - 1:
-                #     neighbours = (
-                #         curr_matrix[i - 1][j - 1]
-                #         + curr_matrix[i][j - 1]
-                #         + curr_matrix[i + 1][j - 1]
-                #         + curr_matrix[i - 1][j]
-                #         + curr_matrix[i + 1][j]
-                #         + curr_matrix[i - 1][0]
-                #         + curr_matrix[i][0]
-                #         + curr_matrix[i + 1][0]
-                #     )
-
-                # else:
-                #     neighbours = (
-                #         curr_matrix[i - 1][j - 1]
-                #         + curr_matrix[i][j - 1]
-                #         + curr_matrix[i + 1][j - 1]
-                #         + curr_matrix[i - 1][j]
-                #         + curr_matrix[i + 1][j]
-                #         + curr_matrix[i - 1][j + 1]
-                #         + curr_matrix[i][j + 1]
-                #         + curr_matrix[i + 1][j + 1]
-                #     )
                 neighbours = neighbour_matrix[i][j]
 
                 if neighbours in birth:
@@ -155,6 +126,8 @@ class Matrix:
                         (j * self.gap_x, i * self.gap_y, self.gap_x, self.gap_y),
                     )
         self.even = not self.even
+
+        """Draw lines after every update."""
         for i in range(self.total_columns):
             gap = i * self.gap_x
             pygame.draw.line(self.WIN, "BLACK", (gap, 0), (gap, self.height))
@@ -175,25 +148,27 @@ class Matrix:
 class Cellular_Automation_Model:
     """
     Moore Rules:
-    B3/S23         Game of Life
-    B2/S           Seed
-    B36/S23        High Life
-    B3678/S34678   Day and Night
-    B35678/S5678   Diamoeba
-    B234/S         Persian Rug
-    B345/S5        Long Life
+    3/23         Game of Life
+    2/           Seed
+    36/23        High Life
+    3678/34678   Day and Night
+    35678/5678   Diamoeba
+    234/         Persian Rug
+    345/5        Long Life
 
     """
 
-    def __init__(self, birth="3", survival="23"):
+    def __init__(self, birth="2", survival=""):
         # TODO Add von Neumann rules.
-        # TODO Add Generation Count
-        # TODO Add Population Count
+        """Editable"""
         self.birth = list(birth)
         self.survival = list(survival)
+        self.delta_time = 500
+
+        """Non Editable"""
         self.model_name = "B" + birth + "/S" + survival
         self.grid = Matrix()
-        self.delta_time = 10
+        self.generation = 0
 
         pygame.display.set_caption("Cellular Automata Model " + self.model_name)
 
@@ -228,9 +203,13 @@ class Cellular_Automation_Model:
                     for event in pygame.event.get():
                         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                             run = False
-                            pygame.display.set_caption("Paused")
+                            pygame.display.set_caption(
+                                f"Paused: Generation = {self.generation} | "
+                                f"Population = {self.grid.get_population(self.generation)}"
+                            )
 
                     self.grid.increment_time_step(self.birth, self.survival)
+                    self.generation += 1
 
 
 if __name__ == "__main__":
@@ -241,6 +220,7 @@ if __name__ == "__main__":
     """
 
     m = Cellular_Automation_Model()
+
     # m.grid.manual_update((180, 100))
     # m.grid.manual_update((180, 200))
     # m.grid.manual_update((180, 300))
